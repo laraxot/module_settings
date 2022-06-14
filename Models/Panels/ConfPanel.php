704bc155096c6d22f03713f436c08b40d5ca7e8c
marco76tv <marco.sottana@gmail.com>
2022-06-14 12:36:12 +0200

    .

--- Models/Panels/ConfPanel.php
+++ Models/Panels/ConfPanel.php
@@ -22,76 +22,26 @@ class ConfPanel extends XotBasePanel {
      */
     public static $title = 'title';
 
-    /**
-     * The columns that should be searched.
-     *
-     * @var array
-     */
-    public static $search = [
-    ];
-
-    /**
-     * The relationships that should be eager loaded on index queries.
-     */
-    public function with(): array {
-        return [];
-    }
-
-    public function search(): array {
-        return [];
-    }
-
-    /**
-     * on select the option id.
-     */
-    public function optionId(object $row) {
-        return $row->area_id;
-    }
-
-    /**
-     * on select the option label.
-     */
-    public function optionLabel(object $row): string {
-        return $row->area_define_name;
-    }
-
-    public function indexNav(): ?\Illuminate\Contracts\Support\Renderable {
-        return null;
-    }
-
-    /**
-     * Build an "index" query for the given resource.
-     *
-     * @param mixed                                 $data
-     * @param \Illuminate\Database\Eloquent\Builder $query
-     *
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public static function indexQuery($data, $query) {
-        // return $query->where('user_id', $request->user()->id);
-        return $query;
-    }
-
     public function fields(): array {
         return [
-            0 => (object) [
+            (object) [
                 'type' => 'String',
                 'name' => 'appname',
                 'rules' => 'required',
                 'comment' => null,
             ],
-            1 => (object) [
+            (object) [
                 'type' => 'String',
                 'name' => 'description',
                 'rules' => 'required',
                 'comment' => null,
             ],
-            2 => (object) [
+            (object) [
                 'type' => 'Text',
                 'name' => 'keywords',
                 'comment' => 'not in Doctrine',
             ],
-            3 => (object) [
+            (object) [
                 'type' => 'Text',
                 'name' => 'author',
                 'comment' => 'not in Doctrine',
